dc1751efafb67a9ed0cdae9144163f6da52705fc
Abhinav-vats <50848068+Abhinav-vats@users.noreply.github.com>
2024-08-05 21:36:46 +0530

    changes for test

--- src/utils.py
+++ src/utils.py
@@ -12,3 +12,16 @@ def save_object(file_path, obj):
             dill.dump(obj, file_obj)
     except Exception as e:
         print(e)
+
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+
+    except Exception as e:
+        print(e)
+
+
+def add(a, b):
+    return a+b
