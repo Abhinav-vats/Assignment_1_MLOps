b2d1541a6508e2ca3b725a98af1ed60505915e8c
Tarak <tarak.singarati@hpe.com>
2024-08-06 14:01:17 +0530

    updated

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,12 @@ WORKDIR /app
 # Copy the current directory contents into the container at /app
 COPY . /app
 
+# Install system dependencies
+RUN apt-get update && apt-get install -y \
+    build-essential \
+    git \
+    && rm -rf /var/lib/apt/lists/*
+
 # Install the dependencies
 RUN pip install --no-cache-dir -r requirements.txt
 
@@ -14,11 +20,12 @@ RUN pip install --no-cache-dir -r requirements.txt
 EXPOSE 5005
 EXPOSE 5000
 
-# Copy the dev_init.sh script
+# Copy the dev_init.sh script and service account JSON file
 COPY src/dev_init.sh /app/dev_init.sh
+COPY /gdrive_service_account.json /app/gdrive_service_account.json
 
 # Make the dev_init.sh script executable
 RUN chmod +x /app/dev_init.sh
 
 # Run the dev_init.sh script and then train_model.py
-CMD ["/bin/bash", "-c", "/app/dev_init.sh && sleep 10 && python /app/train_model.py"]
+CMD ["/bin/bash", "-c", "/app/dev_init.sh && sleep 10 && python /app/src/train_model.py"]
--- src/dev_init.sh
+++ src/dev_init.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-# Initialize DVC without SCM
-dvc init --no-scm
+# Initialize DVC without SCM, force if already exists
+dvc init --no-scm -f
 
-# Add Google Drive remote
-dvc remote add -d myremote gdrive://1CF-7XIaupXxt3EREqhwSOUI0gEvVZAoH
+# Add Google Drive remote, force if already exists
+dvc remote add -d myremote gdrive://1CF-7XIaupXxt3EREqhwSOUI0gEvVZAoH -f
 
-# Configure Google Drive remote
-dvc remote modify myremote gdrive_use_service_account false
-dvc remote modify myremote --local gdrive_client_id 42411527554-l23lu38n6teaeuc9rl6ja2c6jpt6ekqj.apps.googleusercontent.com
-dvc remote modify myremote --local gdrive_client_secret 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
+# Configure Google Drive remote with service account
+dvc remote modify myremote gdrive_use_service_account true
+dvc remote modify myremote --local gdrive_service_account_json /app/gdrive_service_account.json
 
 echo "DVC initialization and configuration completed."
 
 # Start the MLflow server
 mlflow server --backend-store-uri sqlite:///mlflow.db --default-artifact-root ./mlruns --host 0.0.0.0 --port 5000 &
+
+# Wait for a few seconds to ensure MLflow server is up and running
+sleep 10
+
+# Run the model training script
+python /app/src/train_model.py
